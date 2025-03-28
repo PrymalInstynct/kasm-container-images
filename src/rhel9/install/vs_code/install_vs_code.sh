2ae5ed5c55442be2bf8a05dd0073018709b58565
PrymalInstynct <zimzamfam@gmail.com>
2025-03-27 20:59:26 -0600

    update vscode

--- src/rhel9/install/vs_code/install_vs_code.sh
+++ src/rhel9/install/vs_code/install_vs_code.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -ex
-
-# Install vsCode
 ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/x64/g')
-wget -q https://update.code.visualstudio.com/latest/linux-deb-${ARCH}/stable -O vs_code.deb
-apt-get update
-apt-get install -y ./vs_code.deb
 
-# Desktop icon
+wget -q https://update.code.visualstudio.com/latest/linux-rpm-${ARCH}/stable -O vs_code.rpm
+if [[ "${DISTRO}" == @(oracle8|rockylinux9|rockylinux8|oracle9|rhel9|almalinux9|almalinux8|fedora39|fedora40) ]]; then
+  wget -q https://update.code.visualstudio.com/latest/linux-rpm-${ARCH}/stable -O vs_code.rpm
+  dnf localinstall -y vs_code.rpm
+else
+  wget -q https://packages.microsoft.com/yumrepos/vscode/code-1.85.1-1702462241.el7.x86_64.rpm  -O vs_code.rpm
+  yum localinstall -y vs_code.rpm
+fi
 mkdir -p /usr/share/icons/hicolor/apps
 wget -O /usr/share/icons/hicolor/apps/vscode.svg https://kasm-static-content.s3.amazonaws.com/icons/vscode.svg
 sed -i '/Icon=/c\Icon=/usr/share/icons/hicolor/apps/vscode.svg' /usr/share/applications/code.desktop
@@ -15,21 +17,17 @@ sed -i 's#/usr/share/code/code#/usr/share/code/code --no-sandbox##' /usr/share/a
 cp /usr/share/applications/code.desktop $HOME/Desktop
 chmod +x $HOME/Desktop/code.desktop
 chown 1000:1000 $HOME/Desktop/code.desktop
-rm vs_code.deb
+rm vs_code.rpm
 
 # Conveniences for python development
-apt-get update
-apt-get install -y python3-setuptools \
-                   python3-venv \
-                   python3-virtualenv
-
-# Cleanup for app layer
-chown -R 1000:0 $HOME
-find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
-if [ -z ${SKIP_CLEAN+x} ]; then
-  apt-get autoclean
-  rm -rf \
-    /var/lib/apt/lists/* \
-    /var/tmp/* \
-    /tmp/*
+if [[ "${DISTRO}" == @(oracle8|rockylinux9|rockylinux8|oracle9|rhel9|almalinux9|almalinux8|fedora39|fedora40) ]]; then
+  dnf install -y python3-setuptools python3-virtualenv
+  if [ -z ${SKIP_CLEAN+x} ]; then
+    dnf clean all
+  fi
+else
+  yum install -y python3-setuptools python3-virtualenv
+  if [ -z ${SKIP_CLEAN+x} ]; then
+    yum clean all
+  fi
 fi
