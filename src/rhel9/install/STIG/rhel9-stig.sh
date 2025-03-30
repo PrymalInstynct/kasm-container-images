5d8c595be892b5d85f6aa374a2933897b97cade4
PrymalInstynct <zimzamfam@gmail.com>
2025-03-30 07:26:30 -0600

    Updated to apply STIG to base image
    Signed-off-by: PrymalInstynct <zimzamfam@gmail.com>

--- src/rhel9/install/STIG/rhel9-stig.sh
+++ src/rhel9/install/STIG/rhel9-stig.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-git clone https://github.com/ansible-lockdown/RHEL9-STIG.git
-pushd RHEL9-STIG
-ansible-galaxy collection install -r collections/requirements.yml
-ansible-playbook -i inventory.yml site.yml
+if [[ "${DISTRO}" == @(rhel9) ]]; then
+    git clone https://github.com/PrymalInstynct/RHEL9-STIG-MPG.git
+    cp inventory.yml RHEL9-STIG-MPG/
+    pushd RHEL9-STIG-MPG
+    git checkout Mar2025_testing
+    ansible-galaxy collection install -r collections/requirements.yml
+    ansible-playbook -i inventory.yml site.yml
+fi
--- src/rhel9/install/ansible/install_ansible.sh
+++ src/rhel9/install/ansible/install_ansible.sh
@@ -2,7 +2,9 @@
 set -ex
 
 if [[ "${DISTRO}" == @(rhel9) ]]; then
+  dnf -y install git
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   python3 get-pip.py
   pip3 install ansible
+  pip3 install pip-autoremove
 fi
