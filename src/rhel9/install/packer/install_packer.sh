0b986bced9969c28abe1c734b0e034242883813b
PrymalInstynct <zimzamfam@gmail.com>
2025-03-31 05:21:56 -0600

    add packer alias to fix issue with command
    Signed-off-by: PrymalInstynct <zimzamfam@gmail.com>

--- src/rhel9/install/packer/install_packer.sh
+++ src/rhel9/install/packer/install_packer.sh
@@ -9,6 +9,9 @@ rpm --import https://rpm.releases.hashicorp.com/gpg
 if [[ "${DISTRO}" == @(oracle8|rockylinux9|rockylinux8|oracle9|rhel9|almalinux9|almalinux8) ]]; then
   dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
   dnf install -y packer
+  cat <<EOF | tee /etc/profile.d/00-alias.sh
+  alias packer='/usr/bin/packer'
+  EOF
   if [ -z ${SKIP_CLEAN+x} ]; then
     dnf clean all
   fi
