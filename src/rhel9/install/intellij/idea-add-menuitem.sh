d37ba7a2e555cec60c29c691290f7a92a5df9408
PrymalInstynct <zimzamfam@gmail.com>
2025-03-31 05:05:10 -0600

    Add Intellij to Desktop build
    Signed-off-by: PrymalInstynct <zimzamfam@gmail.com>

---
+++ src/rhel9/install/intellij/idea-add-menuitem.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PRG_INSTALL='/opt/idea-IC'
+
+cd "${PRG_INSTALL}"
+
+ICON_NAME=jetbrains-idea
+TMP_DIR=`mktemp --directory`
+DESKTOP_FILE=$TMP_DIR/jetbrains-idea.desktop
+cat << EOF > $DESKTOP_FILE
+[Desktop Entry]
+Version=1.0
+Encoding=UTF-8
+Name=Intellij IDEA
+Keywords=java;intellij;idea
+GenericName=Java Integrated Development Environment
+Type=Application
+Categories=Development;Programming
+Terminal=false
+StartupNotify=true
+Exec="$PRG_INSTALL/idea.sh" %u
+Icon=$ICON_NAME.svg
+EOF
+
+xdg-desktop-menu install $DESKTOP_FILE
+xdg-icon-resource install --size 128 "$PRG_INSTALL/$ICON_NAME.svg" $ICON_NAME
+
+rm $DESKTOP_FILE
+rm -R $TMP_DIR
---
+++ src/rhel9/install/intellij/install_intellij.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
+if [ "${ARCH}" == "arm64" ] ; then
+    echo "Intellij for arm64 currently not supported, skipping install"
+    exit 0
+fi
+if [[ "${DISTRO}" == @(oracle8|rockylinux9|rockylinux8|oracle9|rhel9|almalinux9|almalinux8|fedora39|fedora40) ]]; then
+  curl -fSL https://download.jetbrains.com/idea/ideaIC-2024.3.5.tar.gz -o ideaIC.tar.gz
+  tar xvzf ideaIC.tar.gz -C /opt/idea-IC --strip-components=1
+  wget -O /opt/idea-IC/jetbrains-idea.svg https://resources.jetbrains.com/help/img/idea/2024.3/product.idea.svg
+fi
+chmod +x $HOME/Desktop/jetbrains-idea.desktop
+chown 1000:1000 $HOME/Desktop/jetbrains-idea.desktop
