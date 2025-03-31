82f44a9cf74fff8667fc4924601dfedcbfa1d349
PrymalInstynct <zimzamfam@gmail.com>
2025-03-31 03:49:08 -0600

    Add install of kubectl
    Signed-off-by: PrymalInstynct <zimzamfam@gmail.com>

---
+++ src/rhel9/install/kubectl/install_kubectl.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -ex
+ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
+if [ "${ARCH}" == "arm64" ] ; then
+    echo "Packer for arm64 currently not supported, skipping install"
+    exit 0
+fi
+rpm --import https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key
+if [[ "${DISTRO}" == @(oracle8|rockylinux9|rockylinux8|oracle9|rhel9|almalinux9|almalinux8|fedora39|fedora40) ]]; then
+  cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+  [kubernetes]
+  name=Kubernetes
+  baseurl=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/
+  enabled=1
+  gpgcheck=1
+  gpgkey=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key
+  EOF
+  dnf install -y kubectl
+  if [ -z ${SKIP_CLEAN+x} ]; then
+    dnf clean all
+  fi
+else
+  cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+  [kubernetes]
+  name=Kubernetes
+  baseurl=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/
+  enabled=1
+  gpgcheck=1
+  gpgkey=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key
+  EOF
+  yum install -y kubectl
+  if [ -z ${SKIP_CLEAN+x} ]; then
+    yum clean all
+  fi
+fi
