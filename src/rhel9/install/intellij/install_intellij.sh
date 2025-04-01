9e24d6fe1fa098c23b0a9f83ad259587d6dc8156
PrymalInstynct <zimzamfam@gmail.com>
2025-04-01 06:03:58 -0600

    Updating install of openjdk 24 with intellij
    Signed-off-by: PrymalInstynct <zimzamfam@gmail.com>

--- src/rhel9/install/intellij/install_intellij.sh
+++ src/rhel9/install/intellij/install_intellij.sh
@@ -6,11 +6,13 @@ if [ "${ARCH}" == "arm64" ] ; then
     exit 0
 fi
 if [[ "${DISTRO}" == @(oracle8|rockylinux9|rockylinux8|oracle9|rhel9|almalinux9|almalinux8|fedora39|fedora40) ]]; then
-  dnf -y install java-21-openjdk
-  echo JAVA_HOME='/usr/lib/jvm/java-21-openjdk-21.0.6.0.7-1.el9.x86_64' >> $HOME/.bashrc
   curl -fSL https://download.jetbrains.com/idea/ideaIC-2024.3.5.tar.gz -o ideaIC.tar.gz
   mkdir /opt/idea-IC
-  tar xvzf ideaIC.tar.gz -C /opt/idea-IC --strip-components=1
+  tar xzf ideaIC.tar.gz -C /opt/idea-IC --strip-components=1
+  # Download Latest OpenJDK Release https://jdk.java.net/24/ or Switch to an archive https://jdk.java.net/archive/
+  curl -fSL https://download.java.net/java/GA/jdk24/1f9ff9062db4449d8ca828c504ffae90/36/GPL/openjdk-24_linux-x64_bin.tar.gz -o openjdk-24_linux-x64_bin.tar.gz
+  mkdir -p $HOME/.jdks/openjdk-24
+  tar xzf -C $HOME/.jdks/openjdk-24 --strip-components=1
   cp -r $INST_DIR/rhel9/install/intellij/JetBrains $HOME/.config/
   chown 1000:1000 $HOME/.config/JetBrains
   rm -f ideaIC.tar.gz
