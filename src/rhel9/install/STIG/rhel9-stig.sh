1a2a00d64c351533e026a41eba90c1e01d5bb261
PrymalInstynct <zimzamfam@gmail.com>
2025-04-02 05:31:51 -0600

    Update Inventory and branch for testing
    Signed-off-by: PrymalInstynct <zimzamfam@gmail.com>

--- src/rhel9/install/STIG/rhel9-stig.sh
+++ src/rhel9/install/STIG/rhel9-stig.sh
@@ -5,7 +5,7 @@ if [[ "${DISTRO}" == @(rhel9) ]]; then
     git clone https://github.com/PrymalInstynct/RHEL9-STIG-MPG.git
     cp inventory.yml RHEL9-STIG-MPG/
     pushd RHEL9-STIG-MPG
-    git checkout Mar2025_testing
+    git checkout Apr2025_testing
     ansible-galaxy collection install -r collections/requirements.yml
     ansible-playbook -i inventory.yml site.yml
 fi
